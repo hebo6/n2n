be1ed08aeba5096e0bda47ffc56a95a8dacd39b0
Bo He <qq111372757@gmail.com>
2026-01-01 19:11:30 +0800

    wait after repeated supernode connection failures

--- include/n2n_typedefs.h
+++ include/n2n_typedefs.h
@@ -467,6 +467,7 @@ struct peer_info {
     uint8_t                          local;
     time_t                           uptime;
     n2n_version_t                    version;
+    int                              consecutive_errors;
 
     UT_hash_handle     hh; /* makes this structure hashable */
 };
